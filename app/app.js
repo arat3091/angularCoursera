4ed405eca502de522f4025fb7181d9a148de2f81
Ankur Ratra <ankur.ratra@powerhouse.local>
2016-09-21 11:37:20 +0200

    watchers code

--- app/app.js
+++ app/app.js
@@ -1,8 +1,21 @@
 (function() {
-    var mymod = angular.module('myApp', ['ngMaterial','ngMessages']);
+    var mymod = angular.module('myApp', ['ngMaterial', 'ngMessages']);
 
-    mymod.controller('myController', ['$scope' , function($scope) {
-      $scope.name = "ankur ratra";
+    mymod.controller('myController', ['$scope', function($scope) {
+        $scope.name = "test";
+        $scope.showWatchers = function() {
+            console.log($scope);
+        }
+        $scope.oneConstant = 1;
+
+        $scope.showConstant = function() {
+          $scope.oneConstant++;
+        }
+
+        $scope.$watch('oneConstant', function(newValue,oldValue){
+          console.log("old value :" + oldValue);
+          console.log("new value :" + newValue);
+        })
     }]);
 
 })();
